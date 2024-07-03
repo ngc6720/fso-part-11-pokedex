2e3c9afd913ad131bc180db7474c94757cc44012
ngc6720 <francoiskerforn@gmail.com>
2024-07-03 23:48:27 +0200

    updates

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+    "jest/globals": true,
+    "cypress/globals": true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "jest", "cypress"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    // semi: ["error", "never"],
+    eqeqeq: "error",
+    "no-trailing-spaces": "warn",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    // "no-console": "error",
+    "react/prop-types": 0,
+  },
+};
---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+// get the port from env variable
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("dist"));
+
+app.get("/healthz", (req, res) => {
+  res.send("ok");
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
